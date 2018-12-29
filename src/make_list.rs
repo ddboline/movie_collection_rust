07266f29c6eddf57f44dc32ffa4d35ad4e6179a5
Daniel Boline <ddboline@gmail.com>
2018-12-28 21:32:41 -0500

    finish implementing make-list

--- src/make_list.rs
+++ src/make_list.rs
@@ -4,7 +4,10 @@ extern crate rayon;
 use failure::Error;
 use rayon::prelude::*;
 use std::env::{current_dir, var};
+use std::io::BufRead;
+use std::io::BufReader;
 use std::path::Path;
+use subprocess::Exec;
 
 pub fn map_result_vec<T, E>(input: Vec<Result<T, E>>) -> Result<Vec<T>, E> {
     let mut output: Vec<T> = Vec::new();
@@ -14,7 +17,7 @@ pub fn map_result_vec<T, E>(input: Vec<Result<T, E>>) -> Result<Vec<T>, E> {
     Ok(output)
 }
 
-fn walk_directory(path: &str, match_str: &str) -> Result<Vec<String>, Error> {
+fn walk_directory(path: &str, match_strs: &[String]) -> Result<Vec<String>, Error> {
     Ok(Path::new(path)
         .read_dir()?
         .filter_map(|f| match f {
@@ -23,13 +26,23 @@ fn walk_directory(path: &str, match_str: &str) -> Result<Vec<String>, Error> {
                     let path_name = fpath.path().to_str().unwrap().to_string();
 
                     if ftype.is_dir() {
-                        Some(match walk_directory(&path_name, match_str) {
+                        Some(match walk_directory(&path_name, match_strs) {
                             Ok(v) => v,
                             Err(e) => panic!("{} {}", path_name, e),
                         })
                     } else {
-                        if path_name.contains(match_str) {
-                            Some(vec![path_name])
+                        let path_names: Vec<_> = match_strs
+                            .iter()
+                            .filter_map(|m| {
+                                if path_name.contains(m) {
+                                    Some(path_name.clone())
+                                } else {
+                                    None
+                                }
+                            })
+                            .collect();
+                        if path_names.len() > 0 {
+                            Some(path_names)
                         } else {
                             None
                         }
@@ -60,7 +73,7 @@ fn make_list(do_here: Option<String>) -> Result<(), Error> {
         "/media/seagate4000/Documents/television",
         "/media/seagate4000/television/unwatched",
     ]
-    .iter()
+    .par_iter()
     .filter_map(|d| {
         if Path::new(d).exists() {
             Some(d.to_string())
@@ -79,19 +92,14 @@ fn make_list(do_here: Option<String>) -> Result<(), Error> {
 
     let path = Path::new(&movies_dir);
 
-    let file_list: Vec<_> = path
+    let local_file_list: Vec<_> = path
         .read_dir()?
         .filter_map(|f| match f {
             Ok(fname) => {
                 let file_name = fname.file_name().into_string().unwrap();
-
                 for suffix in &suffixes {
                     if file_name.ends_with(suffix) {
-                        let extern_files: Vec<String> = movie_dirs
-                            .par_iter()
-                            .flat_map(|d| walk_directory(&d, &file_name).unwrap())
-                            .collect();
-                        return Some((file_name, extern_files));
+                        return Some(file_name);
                     }
                 }
                 None
@@ -100,10 +108,72 @@ fn make_list(do_here: Option<String>) -> Result<(), Error> {
         })
         .collect();
 
+    if local_file_list.is_empty() {
+        return Ok(());
+    }
+
+    let file_list: Vec<_> = movie_dirs
+        .par_iter()
+        .flat_map(|d| walk_directory(&d, &local_file_list))
+        .flatten()
+        .collect();
+
     file_list
         .iter()
-        .map(|(f, p)| println!("{} {}", f, p.join(" ")))
+        .map(|f| {
+            let file_name = f.split("/").last().unwrap().to_string();
+            println!("{} {}", file_name, f);
+        })
         .for_each(drop);
+
+    file_list
+        .par_iter()
+        .map(|f| {
+            let command = if f.ends_with(".avi") {
+                format!("aviindex -i {} -o /dev/null", f)
+            } else {
+                format!("ffprobe {} 2>&1", f)
+            };
+
+            let mut timeval = "".to_string();
+
+            let stream = Exec::shell(command).stream_stdout().unwrap();
+            BufReader::new(stream)
+                .lines()
+                .map(|l| {
+                    let items: Vec<_> = l
+                        .unwrap()
+                        .split_whitespace()
+                        .map(|s| s.to_string())
+                        .collect();
+                    if items.len() > 5 {
+                        if items[1] == "V:" {
+                            let nsecs: u64 = items[5]
+                                .trim_start_matches("frames=")
+                                .trim_matches(',')
+                                .parse()
+                                .unwrap();
+                            let nmin = (nsecs as f64 / 60.) as u64;
+                            let nhour = (nmin as f64 / 60.) as u64;
+                            timeval = format!("{:02}:{:02}:{:02}", nhour, nmin, nsecs % 60);
+                        }
+                    }
+                    if items.len() > 1 {
+                        if items[0] == "Duration:" {
+                            let its: Vec<_> = items[1].trim_matches(',').split(":").collect();
+                            let nhour: u64 = its[0].parse().unwrap();
+                            let nmin: u64 = its[1].parse().unwrap();
+                            let nsecs: f64 = its[2].parse().unwrap();
+                            timeval = format!("{:02}:{:02}:{:02}", nhour, nmin, nsecs as u64);
+                        }
+                    }
+                })
+                .for_each(drop);
+
+            println!("{} {}", timeval, f);
+        })
+        .for_each(drop);
+
     Ok(())
 }
 
