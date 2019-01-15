e661dde1133b593ed1f0481fbd2fbd5dd9c03beb
Daniel Boline <ddboline@gmail.com>
2019-01-14 21:55:00 -0500

    ...

--- templates/transcode_script.sh
+++ templates/transcode_script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-nice -n 19 HandBrakeCLI \
-    -i INPUT_FILE -o OUTPUT_FILE \
-    --preset "Android 480p30" \
-    > ~/dvdrip/log/PREFIX_mp4.out 2>&1
+nice -n 19 HandBrakeCLI -i INPUT_FILE -o OUTPUT_FILE --preset "Android 480p30" > ~/dvdrip/log/PREFIX_mp4.out 2>&1
 mv OUTPUT_FILE ~/Documents/movies/
 mv ~/dvdrip/log/PREFIX_mp4.out ~/tmp_avi/
