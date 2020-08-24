e14ad3aadc1817e69ffc1b477592dcf7b31cd002
Daniel Boline <ddboline@gmail.com>
2020-08-24 12:49:13 -0400

    handlebars templates

--- templates/move_script.sh
+++ templates/move_script.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+SHOW="{{SHOW}}"
+FNAME="{{FNAME}}"
+BNAME="{{BNAME}}"
+ONAME="{{ONAME}}"
 
-cp ~/Documents/movies/SHOW.mp4 OUTNAME.mp4.new &
+cp ~/Documents/movies/${SHOW}.mp4 ${OUTNAME}.mp4.new &
 PID1=$!
-if [ -e FNAME ]; then
-    mv FNAME ~/Documents/movies/BNAME.old &
+if [ -e $FNAME ]; then
+    mv $FNAME ~/Documents/movies/${BNAME}.old &
     PID2=$!
     wait $PID2
 fi
 wait $PID1
-if [ -e ~/Documents/movies/BNAME.old ]; then
-    mv ~/Documents/movies/BNAME.old ~/Documents/movies/BNAME
+if [ -e ~/Documents/movies/$BNAME.old ]; then
+    mv ~/Documents/movies/$BNAME.old ~/Documents/movies/$BNAME
 fi
-mv ONAME.mp4.new ONAME.mp4
+mv ${ONAME}.mp4.new ${ONAME}.mp4
 
-make-queue -r FNAME
-make-queue -a ONAME.mp4
+make-queue -r $FNAME
+make-queue -a ${ONAME}.mp4
+make-collection -p
