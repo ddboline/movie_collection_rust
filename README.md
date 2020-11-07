84fbfce89d8652d90132cc4b4a71ca7d62afd898
Daniel Boline <ddboline@gmail.com>
2020-11-07 06:44:01 -0500

    small refactor

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 [![Build Status](https://github.com/ddboline/movie_collection_rust/workflows/Rust/badge.svg?branch=main)](https://github.com/ddboline/movie_collection_rust/actions?branch=main)
 [![codecov](https://codecov.io/gh/ddboline/movie_collection_rust/branch/main/graph/badge.svg)](https://codecov.io/gh/ddboline/movie_collection_rust)
 
-Utilities for maintaining a collection of videos.
+Utilities for maintaining a collection of videos, integrates with [https://trakt.tv](trakt.tv) and has manages transcoding of video files.
+
 
--- src/run_encoding.rs
+++ src/run_encoding.rs
@@ -16,10 +16,10 @@ async fn main() -> Result<(), Error> {
 
     let transcode_service = TranscodeService::new(&config, &config.transcode_queue, &pool, &stdout);
     let transcode_channel = TranscodeChannel::open_channel().await?;
-    transcode_channel.init(&config.transcode_queue).await?;
+    transcode_channel.init(&transcode_service.queue).await?;
     let remcom_service = TranscodeService::new(&config, &config.remcom_queue, &pool, &stdout);
     let remcom_channel = TranscodeChannel::open_channel().await?;
-    remcom_channel.init(&config.remcom_queue).await?;
+    remcom_channel.init(&remcom_service.queue).await?;
 
     let transcode_task = spawn(async move {
         transcode_channel
