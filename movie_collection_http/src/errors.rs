29840e26f4e610513c5bbe933d795a24d673e558
Daniel Boline <ddboline@gmail.com>
2021-02-17 08:36:34 -0500

    handle invalid header

--- movie_collection_http/src/errors.rs
+++ movie_collection_http/src/errors.rs
@@ -7,7 +7,7 @@ use stack_string::StackString;
 use std::{convert::Infallible, fmt::Debug, io::Error as IoError};
 use thiserror::Error;
 use warp::{
-    reject::{MissingCookie, Reject},
+    reject::{InvalidHeader, MissingCookie, Reject},
     Rejection, Reply,
 };
 
@@ -45,6 +45,9 @@ pub async fn error_response(err: Rejection) -> Result<Box<dyn Reply>, Infallible
     if err.is_not_found() {
         code = StatusCode::NOT_FOUND;
         message = "NOT FOUND";
+    } else if err.find::<InvalidHeader>().is_some() {
+        TRIGGER_DB_UPDATE.set();
+        return Ok(Box::new(login_html()));
     } else if let Some(missing_cookie) = err.find::<MissingCookie>() {
         if missing_cookie.name() == "jwt" {
             TRIGGER_DB_UPDATE.set();
