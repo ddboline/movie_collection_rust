a5b1a22692b4729b05aadb257b70797a462a2259
Daniel Boline <ddboline@gmail.com>
2021-07-13 17:17:21 -0400

    rustfmt

--- movie_collection_http/src/movie_queue_app.rs
+++ movie_collection_http/src/movie_queue_app.rs
@@ -28,9 +28,9 @@ use super::{
         movie_queue_delete, movie_queue_play, movie_queue_remcom_directory_file,
         movie_queue_remcom_file, movie_queue_route, movie_queue_show, movie_queue_transcode,
         movie_queue_transcode_cleanup, movie_queue_transcode_directory, movie_queue_transcode_file,
-        movie_queue_transcode_status, movie_queue_update, plex_webhook, refresh_auth,
+        movie_queue_transcode_status, movie_queue_update, plex_events, plex_webhook, refresh_auth,
         trakt_auth_url, trakt_cal, trakt_callback, trakt_watched_action, trakt_watched_list,
-        trakt_watched_seasons, trakt_watchlist, trakt_watchlist_action, tvshows, user, plex_events,
+        trakt_watched_seasons, trakt_watchlist, trakt_watchlist_action, tvshows, user,
     },
 };
 
